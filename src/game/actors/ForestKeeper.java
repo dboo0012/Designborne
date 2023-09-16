78152e4a85a6da8e369ef58d7b45ee08ee52961a
jer-27 <jerrychew010@gmail.com>
2023-09-16 14:10:34 +0800

    (REQ1) Implemented FollowBehaviour to ForestKeeper so that it follows the Player once it is within its surroundings.

--- src/game/actors/ForestKeeper.java
+++ src/game/actors/ForestKeeper.java
@@ -1,29 +1,13 @@
 package game.actors;
-
-import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
-import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.actors.Behaviour;
-import edu.monash.fit2099.engine.displays.Display;
-import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
-import game.actors.behaviours.FollowBehaviour;
-import game.grounds.Void;
 import game.actions.AttackAction;
-import game.actions.VoidDeathAction;
-import game.actors.behaviours.AttackBehaviour;
-import game.actors.behaviours.WanderBehaviour;
-import game.items.HealingVial;
-import game.items.ItemDrop;
-import game.items.OldKey;
-import game.items.RefreshingFlask;
+import game.actors.behaviours.FollowBehaviour;
+import game.attributes.EntityTypes;
+import game.attributes.Status;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * A Forest Keeper  actor that has the ability to be spawned.
@@ -35,6 +19,7 @@ public class ForestKeeper extends EnemyDropActor implements ActorSpawn {
 
     }
 
+
     /**
      * Intrinsic weapon for WanderingUndead
      * @return a freshly-instantiated IntrinsicWeapon
@@ -42,6 +27,16 @@ public class ForestKeeper extends EnemyDropActor implements ActorSpawn {
     @Override
     public IntrinsicWeapon getIntrinsicWeapon() {
         return new IntrinsicWeapon(25, "touches", 75);
+    }
+
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        if (otherActor.hasCapability(EntityTypes.PLAYABLE) && !this.behaviours.containsKey(997)){
+            this.behaviours.put(997, new FollowBehaviour(otherActor));
+        }
+        return super.allowableActions(otherActor, direction, map);
+
+
     }
 
     /**
