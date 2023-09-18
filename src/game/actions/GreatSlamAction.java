4d948bfa5c1c859179e9bb771f27f20d775dfe73
dboo0012 <115628797+dboo0012@users.noreply.github.com>
2023-09-18 18:00:24 +0800

    Complete GiantHammer and GreatSlamAction

--- src/game/actions/GreatSlamAction.java
+++ src/game/actions/GreatSlamAction.java
@@ -4,16 +4,20 @@ import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations;
 import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
 import game.weapons.GiantHammer;
 
-public class GreatSlam extends Action {
+public class GreatSlamAction extends Action {
 
     private final GiantHammer giantHammer;
+    private final Actor target;
 
-    public GreatSlam(GiantHammer giantHammer){
+    public GreatSlamAction(GiantHammer giantHammer, Actor otherActor){
         this.giantHammer = giantHammer;
+        this.target = otherActor;
     }
     @Override
     public String execute(Actor actor, GameMap map) {
@@ -27,17 +31,36 @@ public class GreatSlam extends Action {
             actor.modifyAttribute(BaseActorAttributes.STAMINA, ActorAttributeOperations.DECREASE, (int) requiredStamina);
 
             // Slam
+            slam(target, map);
 
-            return actor + " has activated Great Slam on " + giantHammer.toString();
+            // Attack target
+            String attack = new AttackAction(target, " slams ", giantHammer).attack(actor, map);
+
+            return attack + "\n" + String.format("Great Slam did AOE damage around %s", target);
         }
     }
 
-    public void slam(){
+    public void slam(Actor otherActor, GameMap map){
         // get all the exits of the enemy, within those exits, if there is an actor, attack with 50% damage
+        int originalDamage = giantHammer.damage();
+        int splashDamage = (int) (originalDamage * 0.5);
+
+        Location target = map.locationOf(otherActor); // change the locationOf to the location of the target
+
+        for (Exit exit : target.getExits()) {
+            Location destination = exit.getDestination();
+
+            if (destination.containsAnActor()) {
+                Actor affectedActor = destination.getActor();
+                affectedActor.hurt(splashDamage);
+                new Display().println(String.format("%s is splashed by %d damage from Great Slam", affectedActor, splashDamage));
+            }
+        }
     }
 
     @Override
     public String menuDescription(Actor actor) {
         return actor + " activates the skill of Great Slam on " + giantHammer.toString();
     }
+
 }
--- src/game/weapons/GiantHammer.java
+++ src/game/weapons/GiantHammer.java
@@ -5,10 +5,11 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Location;
 import edu.monash.fit2099.engine.weapons.WeaponItem;
 import game.actions.AttackAction;
-import game.actions.GreatSlam;
+import game.actions.GreatSlamAction;
 
 public class GiantHammer extends WeaponItem {
-    private ActionList actions = new ActionList();
+    private ActionList myActions = new ActionList();
+    private GreatSlamAction greatSlamAction;
     /**
      * Constructor.
      */
@@ -20,30 +21,35 @@ public class GiantHammer extends WeaponItem {
      * Add a new StabStepAction to the weapon.
      * @param newAction the new action to be added.
      */
-    public void addAction(GreatSlam newAction){
-        actions.add(newAction);
-    }
+//    public void addAction(GreatSlam newAction){
+//        myActions.add(newAction);
+//        this.greatSlam = newAction;
+//    }
 
     /**
      * Adds the focusAction to the allowable actions.
      * @param actor the actor that owns the item
      * @return ActionList of allowable actions
      */
-    @Override
-    public ActionList allowableActions(Actor actor) {
-        return actions;
-    }
+//    @Override
+//    public ActionList allowableActions(Actor actor) {
+//        myActions.add(greatSlam);
+//        return myActions;
+//    }
 
     /**
      * Adds the attack action to attack mobs.
-     * @param actor the other actor
+     * @param otherActor the other actor
      * @param location the location of the other actor
      * @return ActionList of allowable actions
      */
     @Override
-    public ActionList allowableActions(Actor actor, Location location) {
+    public ActionList allowableActions(Actor otherActor, Location location) {
         ActionList actions = new ActionList();
-        actions.add(new AttackAction(actor, location.toString(), this));
+        actions.add(new AttackAction(otherActor, location.toString(), this));
+//        greatSlamAction = new GreatSlamAction(this, otherActor);
+//        actions.add(greatSlamAction);
+        actions.add(new GreatSlamAction(this, otherActor));
         return actions;
     }
 }
