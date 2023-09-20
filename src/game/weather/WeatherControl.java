fffdf0a309dc7302c99fdc95e68ff71bfcb30d2c
jer-27 <jerrychew010@gmail.com>
2023-09-20 14:04:15 +0800

    (REQ5) Added Javadoc and in-line comments for WeatherControl class.

--- src/game/weather/WeatherControl.java
+++ src/game/weather/WeatherControl.java
@@ -1,28 +1,45 @@
 package game.weather;
 
+/**
+ * A class that controls the game's weather conditions.
+ */
 public class WeatherControl {
     private static Weather currentWeather;
     private static int turnsSinceLastChange;
     private static final int TURNS_PER_WEATHER_CHANGE = 3; // Change weather every 3 turns
 
+    /**
+     * Initializes the WeatherControl with an initial sunny weather condition.
+     */
     public WeatherControl() {
         currentWeather = Weather.SUNNY; // Initial weather condition
         turnsSinceLastChange = 0;
     }
 
+    /**
+     * Gets the current weather condition.
+     *
+     * @return The current weather condition (either SUNNY or RAINY).
+     */
     public static Weather getCurrentWeather() {
         return currentWeather;
     }
 
+    /**
+     * Updates the game's weather condition based on the number of turns.
+     */
     public void updateWeather() {
         turnsSinceLastChange++;
         if (turnsSinceLastChange >= TURNS_PER_WEATHER_CHANGE) {
             // Change the weather
             toggleWeather();
             turnsSinceLastChange = 0;
+            }
         }
-    }
 
+    /**
+     * Toggles between SUNNY and RAINY weather conditions.
+     */
     private void toggleWeather() {
         // Toggle between SUNNY and RAINY
         currentWeather = (currentWeather == Weather.SUNNY) ? Weather.RAINY : Weather.SUNNY;
