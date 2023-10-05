a0b6e75f2bed8f900a2ddcc76fe646acde57700a
szai0011 <szai0011@student.monash.edu>
2023-10-05 18:10:26 +0800

    Add Design Rationale for base code

--- src/game/actors/HollowSoldier.java
+++ src/game/actors/HollowSoldier.java
@@ -1,28 +1,9 @@
 package game.actors;
 
-import edu.monash.fit2099.engine.actions.Action;
-import edu.monash.fit2099.engine.actions.ActionList;
-import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.actors.Behaviour;
-import edu.monash.fit2099.engine.displays.Display;
-import edu.monash.fit2099.engine.items.Item;
-import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
-import game.grounds.Void;
-import game.actions.AttackAction;
-import game.actions.VoidDeathAction;
-import game.actors.behaviours.AttackBehaviour;
-import game.actors.behaviours.WanderBehaviour;
 import game.items.HealingVial;
-import game.items.ItemDrop;
-import game.items.OldKey;
 import game.items.RefreshingFlask;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * A Hollow Soldier actor that has the ability to be spawned.
