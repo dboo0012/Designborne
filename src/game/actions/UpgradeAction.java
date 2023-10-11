9c3fa672aaf2dd55493255d6010490dce7ff9a06
dboo0012 <115628797+dboo0012@users.noreply.github.com>
2023-10-11 16:35:13 +0800

    added Blacksmith class

---
+++ src/game/actions/UpgradeAction.java
@@ -0,0 +1,18 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+
+public class UpgradeAction extends Action {
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        return null;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return null;
+    }
+}
---
+++ src/game/actors/Blacksmith.java
@@ -0,0 +1,51 @@
+package game.actors;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actions.DoNothingAction;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.actions.SellAction;
+import game.attributes.Ability;
+import game.attributes.EntityTypes;
+import game.items.Tradeable;
+
+public class Blacksmith extends Actor {
+    public Blacksmith() {
+        super("Blacksmith", 'B', 1);
+    }
+
+    @Override
+    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        return new DoNothingAction();
+    }
+
+    public ActionList getItems(Actor sellingActor){
+        ActionList actions = new ActionList(); //List of actions
+
+        for (int i = 0; i < sellingActor.getItemInventory().size(); i++){ // For each item to sell
+            Item item = sellingActor.getItemInventory().get(i); // Get Item
+
+            if (item.hasCapability(Ability.UPGRADE)){ // IF that item is tradeable
+//                Tradeable tradeableItem = ((Tradeable) item); //[Code Smell (Downcasting)]
+//                int price = tradeableItem.getPrice(); // Get price
+//                actions.add(new SellAction(item, tradeableItem, price, sellingActor)); // Create upgrade action
+            }
+        }
+
+        return actions;
+    }
+
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        ActionList actions = super.allowableActions(otherActor, direction, map);
+
+        if (otherActor.hasCapability(EntityTypes.PLAYABLE)){ // Only player can upgrade items
+            actions.add(getItems(otherActor)); // Add the items that the player can upgrade
+        }
+
+        return actions;
+    }
+}
